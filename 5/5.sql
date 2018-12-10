de48e5db398729f4534f61aeb0899ebacc4a5588
Artur Gudiev <Artur.Gudiev@emc.com>
2018-12-10 09:33:06 +0300

    Finish 5.sql script

--- 5/5.sql
+++ 5/5.sql
@@ -64,4 +64,6 @@ prompt ----------------after dropping --------------------------------
 
 select COUNT(1) from all_tables where upper(owner)='MYSCHEMA';
 
---host C:\oraclexe\app\oracle\product\11.2.0\server\bin\expdp.exe system/oracle schemas=myschema directory=dp_dir dumpfile=expdp_myschema.dmp logfile=expdp_myschema.log
+host C:\oraclexe\app\oracle\product\11.2.0\server\bin\impdp.exe impdp system/oracle schemas=myschema directory=dp_dir dumpfile=expdp_myschema.dmp
+
+select COUNT(1) from all_tables where upper(owner)='MYSCHEMA';
